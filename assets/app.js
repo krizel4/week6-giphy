df11acc6166e23dd33907b045b8e2161225adfaa
Krizel <krizzlybear@Nathans-MacBook-Pro.local>
2019-11-13 21:12:50 -0800

    adding pseudocode to start the process

---
+++ assets/app.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    // Array of initial buttons
+    var preselectedComedies = ["The Office", "Catastrophe", "Parks and Rec", "Arrested Development", "Brooklyn 99", "SNL", "Silicon Valley", "30 Rock", "The Last Man on Earth", "Mindy Project"];
+    
+    // AJAX - GIPHY call using the GET method
+    
+    // Create a loop that appends a button for each string in the array
+
+    // On click, page should grab 10 static, non-animated gif images
+
+    // When user clicks a still GIPHY, gif should animate. 
+
+    // When user clicks a moving gif, it should stop animating.
+
+    // Add a rating to each gif
+
+    // When a user searches using the input form, add the input to the array.
+
+    // Call each topic chosen by the user and make a button on the page. (Like Activity 6.7)
+
+    //    * Be sure to read about these GIPHY parameters (hint, hint):
+    //      * `q`
+    //      * `limit`
+    //      * `rating`
+    
+    
+});
